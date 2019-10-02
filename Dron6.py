2ee3245d81b298f0a7e1fe87e92b0006f17ed038
Bysi4ka <ivansuper414@gmail.com>
2019-10-02 22:05:52 +0300

    recognize sky, isn't, wet. woter.

--- Dron6.py
+++ Dron6.py
@@ -1,9 +1,28 @@
 from PIL import Image
 
-r = 30
+r = 50
+def name(color):
+    a, b, c = color
+    max=a
+    if(b>max):
+        max =b
+    if(c>max):
+        max =c
+    if(c-a>=50 or c-b>=50):
+        return "sky"
+    elif (a>=135 and a<=165 and b>=105 and b<=130 and c<95):
+        return "woter,brown"
+    elif(max>209 and max-b<=r and max-c<=r):
+        return "sky"
+    elif(max<=100 and max-b<=r and max-c<=r):
+        return "wet"
+    elif(max<=209 and max-b<=r and max-c<=r):
+        return "woter"
+    else:
+        return "isn't"
 
-def get_main_color(file):
-    img = Image.open(file)
+
+def get_main_color(img):
     colors = img.getcolors(2560) #put a higher value if there are many colors in your image
     max_occurence, most_present = 0, 0
     try:
@@ -14,11 +33,11 @@ def get_main_color(file):
     except TypeError:
         raise Exception("Too many colors in the image")
 
-img = Image.open("Photo.png")
+img = Image.open("Photo.jpg")
 imgwidth, imgheight = img.size
 #img.crop((30, 30, w-80, h-40)).save("file.png")
 amount = 1;
-width, length = 20, 20
+width, length = 15, 10
 
 for i in range(0,imgheight,length):
     for j in range(0,imgwidth,width):
@@ -31,28 +50,12 @@ for i in range(0,imgheight,length):
         else:
             h = i+length
         
-        img.crop((j, i, w, h)).save("images/file"+str(amount)+".png")
+        #img.crop((j, i, w, h)).save("images/file"+str(amount)+".png")
+        cropimages=img.crop((j, i, w, h))
+        color = get_main_color(cropimages)
+        imagename=name(color)
+        cropimages.save("images/"+str(amount)+str(imagename)+str(color)+".png")
         amount=amount+1
 print(amount)
        
 
-
-for q in range(1,amount,1):
-    a, b, c = (get_main_color("images/file"+str(q)+".png"))
-    max=a
-    if(b>max):
-        max =b
-    if(c>max):
-        max =c
-    if(a==b and a==c and c==b and a<=56):
-        print("wet")
-    elif (a==b and a==c and c==b and a>56 and a<209):
-        print("woter")
-    elif(max>209 and max-b<=r and max-c<=r):
-        print("sky")
-    elif(max<=56 and max-b<=r and max-c<=r):
-        print("wet")
-    elif(max<=209 and max-b<=r and max-c<=r):
-        print("woter")
-    else:
-        print("isn't sky and isn't field ")
